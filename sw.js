25b18691a17672de0339d0ec96453c1ff6730e44
felix <57463186+ijustcreate@users.noreply.github.com>
2026-08-07 22:40:11 -0700

    Add optional karaoke host mode

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='bcd-karaoke-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='bcd-karaoke-v2';
+self.addEventListener('install',event=>event.waitUntil(caches.open(C).then(cache=>cache.addAll(['./','./index.html'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(C).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));return}event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)))});
