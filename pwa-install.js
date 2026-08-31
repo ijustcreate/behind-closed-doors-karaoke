dc6b04fa139b8ba17876f3629e372820a83c1277
felix <57463186+ijustcreate@users.noreply.github.com>
2026-08-30 22:43:11 -0700

    Stretch venue header copy vertically

--- pwa-install.js
+++ pwa-install.js
@@ -9,7 +9,7 @@ if (!document.getElementById('bcdkc-member-touch-icon')) {
 if (!document.getElementById('bcd-info-script')) {
   const bcdInfoScript = document.createElement('script');
   bcdInfoScript.id = 'bcd-info-script';
-  bcdInfoScript.src = 'bcd-info.js?v=20260830-full-copy-panel';
+  bcdInfoScript.src = 'bcd-info.js?v=20260830-stretched-copy';
   document.head.append(bcdInfoScript);
 }
 
