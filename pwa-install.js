bee5614485b65c65a70b5295bb05c423c122c7dd
felix <57463186+ijustcreate@users.noreply.github.com>
2026-08-30 22:48:13 -0700

    Reduce venue header height

--- pwa-install.js
+++ pwa-install.js
@@ -9,7 +9,7 @@ if (!document.getElementById('bcdkc-member-touch-icon')) {
 if (!document.getElementById('bcd-info-script')) {
   const bcdInfoScript = document.createElement('script');
   bcdInfoScript.id = 'bcd-info-script';
-  bcdInfoScript.src = 'bcd-info.js?v=20260830-stretched-copy';
+  bcdInfoScript.src = 'bcd-info.js?v=20260830-compact-height';
   document.head.append(bcdInfoScript);
 }
 
